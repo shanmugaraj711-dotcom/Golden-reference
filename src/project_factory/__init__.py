ca6a076a324770ae0928884ca1b7e8c0ee47cffa
shanmugaraj711-dotcom <shanmugaraj711@gmail.com>
2026-08-22 00:46:58 +0530

    feat: export factory runner

--- src/project_factory/__init__.py
+++ src/project_factory/__init__.py
@@ -2,12 +2,14 @@
 
 from .agent_adapter import AgentResult, AgentTask, CodexCliAgent, CodingAgent
 from .engine import ProjectFactory, RunResult
+from .runner import FactoryRunner
 
 __all__ = [
     "AgentResult",
     "AgentTask",
     "CodexCliAgent",
     "CodingAgent",
+    "FactoryRunner",
     "ProjectFactory",
     "RunResult",
 ]
